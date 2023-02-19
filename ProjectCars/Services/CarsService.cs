fc29118567bc873b37e8e31696b1b1a740564253
Brady Albaugh <94060840+BradyAlbaugh@users.noreply.github.com>
2023-02-19 14:46:27 -0600

    Add files via upload

---
+++ ProjectCars/Services/CarsService.cs
@@ -0,0 +1,29 @@
+﻿using ProjectCars.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace ProjectCars.Services;
+
+public class CarsService
+{
+    private readonly IMongoCollection<Cars> _carsCollection;
+
+    public CarsService(IOptions<CarsDatabaseSettings> carsDatabaseSettings)
+    {
+        var mongoClient = new MongoClient(carsDatabaseSettings.Value.ConnectionString);
+
+        var mongoDatabase = mongoClient.GetDatabase(carsDatabaseSettings.Value.DatabaseName);
+
+        _carsCollection = mongoDatabase.GetCollection<Cars>(carsDatabaseSettings.Value.CarsCollectionName);
+    }
+
+    public async Task<List<Cars>> GetAsync() => await _carsCollection.Find(_ => true).ToListAsync();
+
+    public async Task<Cars?> GetAsync(string id) => await _carsCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+    public async Task CreateAsync(Cars newCars) => await _carsCollection.InsertOneAsync(newCars);
+
+    public async Task UpdateAsync(string id, Cars updatedCars) => await _carsCollection.ReplaceOneAsync(x => x.Id == id, updatedCars);
+
+    public async Task RemoveAsync(string id) => await _carsCollection.DeleteOneAsync(x => x.Id == id);
+}
---
+++ ProjectCars/Services/OwnerService.cs
@@ -0,0 +1,29 @@
+﻿using ProjectCars.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace ProjectCars.Services;
+
+public class OwnerService
+{
+    private readonly IMongoCollection<Owner> _ownerCollection;
+
+    public OwnerService(IOptions<OwnerDatabaseSettings> ownerDatabaseSettings)
+    {
+        var mongoClient = new MongoClient(ownerDatabaseSettings.Value.ConnectionString);
+
+        var mongoDatabase = mongoClient.GetDatabase(ownerDatabaseSettings.Value.DatabaseName);
+
+        _ownerCollection = mongoDatabase.GetCollection<Owner>(ownerDatabaseSettings.Value.OwnerCollectionName);
+    }
+
+    public async Task<List<Owner>> GetAsync() => await _ownerCollection.Find(_ => true).ToListAsync();
+
+    public async Task<Owner?> GetAsync(string id) => await _ownerCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+    public async Task CreateAsync(Owner newOwner) => await _ownerCollection.InsertOneAsync(newOwner);
+
+    public async Task UpdateAsync(string id, Owner updatedOwner) => await _ownerCollection.ReplaceOneAsync(x => x.Id == id, updatedOwner);
+
+    public async Task RemoveAsync(string id) => await _ownerCollection.DeleteOneAsync(x => x.Id == id);
+}
